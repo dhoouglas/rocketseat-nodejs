5f47836755e85f1c6afe024162815c3a42c6d054
dhoouglas <dhoouglas@hotmail.com>
2025-03-31 11:25:08 -0300

    feat: implemented insert users and hash to passwords

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,13 +1,7 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-
-prisma.user.create({
-  data: {
-    name: "Douglas Lima",
-    email: "douglas@email.com",
-  },
-});
+app.register(appRoutes);
