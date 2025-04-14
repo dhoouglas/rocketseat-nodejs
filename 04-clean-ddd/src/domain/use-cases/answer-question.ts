c8fa4a67cdc97ef4fcb8baed754d9e47ceeda984
dhoouglas <dhoouglas@hotmail.com>
2025-04-14 11:54:18 -0300

    feat: abstracting entity creation

--- 04-clean-ddd/src/domain/use-cases/answer-question.ts
+++ 04-clean-ddd/src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEtityId } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
@@ -15,10 +16,10 @@ export class AnswerQuestionUseCase {
     questionId,
     content,
   }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId,
+      authorId: new UniqueEtityId(instructorId),
+      questionId: new UniqueEtityId(questionId),
     });
 
     await this.answersRepository.create(answer);
