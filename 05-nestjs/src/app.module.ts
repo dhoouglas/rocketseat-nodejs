2ac0f05016e17272b75e742b22f2bf29c3fa4c7b
dhoouglas <dhoouglas@hotmail.com>
2025-05-12 11:40:07 -0300

    feat: creating prisma service

--- 05-nestjs/src/app.module.ts
+++ 05-nestjs/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService, AppService], // PrismaService PRIMEIRO
 })
 export class AppModule {}
