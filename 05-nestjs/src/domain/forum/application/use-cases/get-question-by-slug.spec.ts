bec56c2a86f52992007657ec03cc93b8b2e6bef9
dhoouglas <dhoouglas@hotmail.com>
2025-05-27 14:49:55 -0300

    test: testing return of question details

--- 05-nestjs/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ 05-nestjs/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,38 +1,73 @@
-import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
-import { makeQuestion } from 'test/factories/make-question'
-import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { makeQuestion } from "test/factories/make-question";
+import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-students-repository";
+import { InMemoryAttachmentsRepository } from "test/repositories/in-memory-attachments-repository";
+import { makeStudent } from "test/factories/make-student";
+import { makeAttachment } from "test/factories/make-attachment";
+import { makeQuestionAttachment } from "test/factories/make-question-attachments";
 
-let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
-let inMemoryQuestionsRepository: InMemoryQuestionsRepository
-let sut: GetQuestionBySlugUseCase
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository;
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: GetQuestionBySlugUseCase;
 
-describe('Get Question By Slug', () => {
+describe("Get Question By Slug", () => {
   beforeEach(() => {
     inMemoryQuestionAttachmentsRepository =
-      new InMemoryQuestionAttachmentsRepository()
+      new InMemoryQuestionAttachmentsRepository();
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository();
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
       inMemoryQuestionAttachmentsRepository,
-    )
-    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
-  })
+      inMemoryAttachmentsRepository,
+      inMemoryStudentsRepository
+    );
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
+  });
 
-  it('should be able to get a question by slug', async () => {
+  it("should be able to get a question by slug", async () => {
+    const student = makeStudent({ name: "John Doe" });
+
+    await inMemoryStudentsRepository.create(student);
+
     const newQuestion = makeQuestion({
-      slug: Slug.create('example-question'),
-    })
+      authorId: student.id,
+      slug: Slug.create("example-question"),
+    });
 
-    await inMemoryQuestionsRepository.create(newQuestion)
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    const attachment = makeAttachment({
+      title: "Some attachment",
+    });
+
+    inMemoryAttachmentsRepository.items.push(attachment);
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        attachmentId: attachment.id,
+        questionId: newQuestion.id,
+      })
+    );
 
     const result = await sut.execute({
-      slug: 'example-question',
-    })
+      slug: "example-question",
+    });
 
     expect(result.value).toMatchObject({
       question: expect.objectContaining({
         title: newQuestion.title,
+        author: "John Doe",
+        attachments: [
+          expect.objectContaining({
+            title: attachment.title,
+          }),
+        ],
       }),
-    })
-  })
-})
+    });
+  });
+});
